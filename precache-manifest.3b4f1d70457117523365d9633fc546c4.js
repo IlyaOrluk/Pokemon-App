d9e8752e568153aee8556f6a39498875f5979eea
IlyaOrluk <misfits120@mail.ru>
2019-07-30 00:26:09 +0300

    Updates

---
+++ precache-manifest.3b4f1d70457117523365d9633fc546c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b5551ba10f013a335e91b98f530d51",
+    "url": "/Pokemon-App/static/media/bg1.26b5551b.png"
+  },
+  {
+    "revision": "24d445d3033c4d289c72",
+    "url": "/Pokemon-App/static/js/runtime~main.24d445d3.js"
+  },
+  {
+    "revision": "937511d28d8015931b16",
+    "url": "/Pokemon-App/static/js/main.937511d2.chunk.js"
+  },
+  {
+    "revision": "901b0293cf3d7b82acdd",
+    "url": "/Pokemon-App/static/js/1.901b0293.chunk.js"
+  },
+  {
+    "revision": "937511d28d8015931b16",
+    "url": "/Pokemon-App/static/css/main.0bb0f0da.chunk.css"
+  },
+  {
+    "revision": "b9e932126aad60647e20440d67d41e17",
+    "url": "/Pokemon-App/index.html"
+  }
+];
